47b2f3d526ad00b0981b464a7c1764cad8463d30
priya <89237530+sparklipriya@users.noreply.github.com>
2023-11-05 22:01:08 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import stacknavigator from './stacknavigator';
 import { NavigationContainer } from '@react-navigation/native';
 import { useTailwind } from 'tailwind-rn';
+apiKey = process.env.REACT_APP_TASK
 const App = () => {
   const tw = useTailwind();
   <View style={ tw( "flex-1 justify-center items-center")}>
